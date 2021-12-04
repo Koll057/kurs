2463b587b7af3ca2f950739ce28d2e582a69f8d1
Sproncho <dan.zaborov@gmail.com>
2021-12-04 16:01:18 +0300

    id

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import './Card.css';
 
-export default function Card({name,img}){
-  return <div className="body_card">
+export default function Card({name,img,click}){
+  return <div className="body_card" onClick={()=>{click()}}>
       <div className="img_card">
         <img src={img} alt="" height="100%" width="100%"/>
       </div>
