2a40d94df3f0497213ef655b946faa723d386670
Sproncho <dan.zaborov@gmail.com>
2021-12-04 14:54:02 +0300

    bars

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import './Card.css';
 
-export default function Card({nameC,imgC}){
+export default function Card({name,img}){
   return <div className="body_card">
       <div className="img_card">
-        <img src={imgC} alt="" height="100%" width="100%"/>
+        <img src={img} alt="" height="100%" width="100%"/>
       </div>
       <div className="name_card">
-        {nameC}
+        {name}
       </div>
    </div>
 }
